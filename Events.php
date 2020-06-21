5ad149a8591d69635c83f9e71e921474a3581dca
MasterMindNET <romazahrypa@gmail.com>
2020-06-21 14:55:41 +0300

    fix: sortOrder removed from gallery/Events.php lines 40-42; sortOrder placed in onSpaceSidebarInit

--- Events.php
+++ Events.php
@@ -28,18 +28,12 @@ class Events
         try {
             if ($event->sender->space !== null && $event->sender->space->isModuleEnabled('gallery')) {
 
-                /** @var Module $module */
-                $module = Yii::$app->getModule('gallery');
-
                 $event->sender->addItem([
                     'label' => Yii::t('GalleryModule.base', 'Gallery'),
                     'group' => 'modules',
                     'url' => Url::toGalleryOverview($event->sender->space),
                     'icon' => '<i class="fa fa-picture-o"></i>',
                     'isActive' => (Yii::$app->controller->module && Yii::$app->controller->module->id == 'gallery'),
-                    'sortOrder' => (int) $module->settings
-                        ->contentContainer($event->sender->space)
-                        ->get(ContainerSettings::SETTING_SORT_ORDER)
                 ]);
             }
         } catch (\Throwable $e) {
@@ -66,8 +60,11 @@ class Events
     public static function onSpaceSidebarInit($event)
     {
         try {
+            /** @var Module $module */
+            $module = Yii::$app->getModule('gallery');
+
             if ($event->sender->space !== null && $event->sender->space->isModuleEnabled('gallery')) {
-                $event->sender->addWidget(GallerySnippet::class, ['contentContainer' => $event->sender->space], ['sortOrder' => 0]);
+                $event->sender->addWidget(GallerySnippet::class, ['contentContainer' => $event->sender->space], ['sortOrder' => (int) $module->settings->contentContainer($event->sender->space)->get(ContainerSettings::SETTING_SORT_ORDER)]);
             }
         } catch (\Throwable $e) {
             Yii::error($e);
